1e720309203640f304733c13a5b6150a42c2b771
songwuk <1017301325@qq.com>
2022-07-13 18:08:46 +0800

    init: add

---
+++ src/pages/000.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+onMounted(() => {
+  const canvas = document.querySelector('.canvas') as HTMLCanvasElement
+  const ctx = canvas.getContext('2d')!
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  ctx.fillStyle = 'red'
+  ctx.fillRect(0, 0, 150, 150)
+
+  ctx.fillStyle = 'blue'
+  ctx.arc(230, 50, 50, 0, Math.PI * 2, true)
+  ctx.fill()
+
+  ctx.beginPath()
+  ctx.fillStyle = 'green'
+  ctx.moveTo(320, 25)
+  ctx.lineTo(475, 25)
+  ctx.lineTo(320, 105)
+
+  ctx.fill()
+})
+</script>
+
+<template>
+  <canvas class="canvas" />
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div w150 h150 b-gray-700 b class="square">
+    si
+  </div>
+</template>
+
+<style>
+  .square{
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+  }
+</style>
